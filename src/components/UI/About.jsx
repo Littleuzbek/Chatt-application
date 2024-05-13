4efc0a93f1098823d52f9b3675724f581232a937
Littleuzbek <dilshod.akbarov.2003@gmail.com>
2024-05-13 16:39:02 +0500

    update

--- src/components/UI/About.jsx
+++ src/components/UI/About.jsx
@@ -7,39 +7,38 @@ import { FaAngleDown } from "react-icons/fa";
 import { CgClose } from "react-icons/cg";
 import { useDispatch, useSelector } from "react-redux";
 import { uiActions } from "../../redux/uiSlice";
-import {  doc, getDoc,  } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import Media from "./Media";
 
 export default function About() {
   const [chosenUser, setChosenUser] = useState();
-  const [media,setMedia] = useState();
+  const [media, setMedia] = useState();
   const [toggleSection, setToggleSection] = useState(false);
   const dispatch = useDispatch();
   const user = useSelector((state) => state.chat.user);
   const chatId = useSelector((state) => state.chat.chatId);
-console.log(user);
+  console.log(user);
+
   useEffect(() => {
-    const fetchUserData = async () => {
-      await getDoc(doc(db, "users", user.uid)).then((res) =>{
+    const FetchUserData = async () => {
+      await getDoc(doc(db, "users", user.uid)).then((res) => {
         setChosenUser(res.data());
         console.log(res);
-      }
-      );
+      });
     };
-      const searchMedia = async() =>{
-        await getDoc(doc(db,'chats', chatId)).then(res=>{
-          setMedia(res.data()?.messages)
-          console.log(res.data()?.messages);
-        });
-      }
+    const SearchMedia = async () => {
+      await getDoc(doc(db, "chats", chatId)).then((res) => {
+        setMedia(res.data()?.messages);
+        console.log(res.data()?.messages);
+      });
+    };
 
     return () => {
-      searchMedia()
-      fetchUserData()
+      FetchUserData();
+      SearchMedia();
     };
-  }, [user,chatId]);
-console.log(chosenUser);
-console.log(media);
+  }, [user, chatId]);
+
   return (
     <Fragment>
       <Backdrop />
@@ -55,15 +54,15 @@ console.log(media);
             <div className="infO">
               <div>
                 <p>Name</p>
-                <p>{user?.displayName ? user?.displayName : '...'}</p>
+                <p>{user?.displayName ? user?.displayName : "..."}</p>
               </div>
               <div>
                 <p>Username</p>
-                <p>{chosenUser?.username ? chosenUser?.username : '...'}</p>
+                <p>{chosenUser?.username ? chosenUser?.username : "..."}</p>
               </div>
               <div>
                 <p>About</p>
-                <p>{chosenUser?.about ? chosenUser?.about : '...'}</p>
+                <p>{chosenUser?.about ? chosenUser?.about : "..."}</p>
               </div>
               <FaAngleDown
                 className="toMedia"
@@ -83,8 +82,8 @@ console.log(media);
                 <div>Files</div>
               </div>
               <div className="itemS">
-                {media?.map((m) =>(
-                  <Media src={m}/>
+                {media?.map((m) => (
+                  <Media src={m} />
                 ))}
               </div>
             </div>
